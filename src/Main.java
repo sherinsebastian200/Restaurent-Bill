7f7d44db2c96ee2c7b261141f6c258a66e4f035b
sherin200 <sherinsebastian21>
2022-11-22 13:00:03 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,30 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 class customer {
-    int tea;
-    int coffee;
-    int snacks;
-    int idli;
-    int dosa;
+    int tea=10;
+    int coffee=15;
+    int snacks=10;
+    int idli=8;
+    int dosa=6;
     String name;
     long phoneno;
+    int total;
+    public customer()
+    {
+
+    }
 
     public customer(String name, long phoneno) {
         this.name = name;
         phoneno = phoneno;
     }
 
-    int tea(int count) {
+    public void tea(int count) {
         this.tea = this.tea * count;
-        return this.tea;
+        System.out.println("total"+this.tea);
+
+
+
 
 
     }
@@ -40,20 +48,57 @@ class customer {
         return this.dosa;
 
     }
+    public int getTea() {
+        return tea;
 
+    }
 
+    public int getCoffee() {
+        return coffee;
+    }
 
+    public int getSnacks() {
+        return snacks;
+    }
 
+    public int getIdli() {
+        return idli;
+    }
 
+    public int getDosa() {
+        return dosa;
+    }
 }
+
+
+
+
+
 public class Main {
     public static void main(String[] args) {
         ArrayList<customer> arr = new ArrayList<customer>();
         while (true) {
             System.out.println("SELECT ITEM \n 1 - Tea \n 2 - coffee \n 3 - snacks \n 14 - idli \n 5 - dosa \n 6- Generate bill \n 7-view trasactions\n 8-Exit");
-            Scanner input = new Scanner(System.in);
-            int n = input.nextInt();
+            Scanner sc = new Scanner(System.in);
+            customer c = new customer();
+
+            int n = sc.nextInt();
             switch (n) {
+
+                case 1:
+                    System.out.println("Enter the quantities");
+                    System.out.println("enter the count");
+                    int q=sc.nextInt();
+                    System.out.println("enter name");
+                    String name=sc.next();
+                    System.out.println("enter phone number");
+                    long number=sc.nextLong();
+                    c.tea(q);
+                    break;
+
+
+
+
 
             }
         }
